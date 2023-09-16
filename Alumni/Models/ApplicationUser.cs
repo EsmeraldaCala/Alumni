06cf50b5b9ae841579123f9ec509d8417738164a
Flavio Kaja <Flavio.KajaFK@gmail.com>
2023-09-16 16:06:13 +0200

    Add project files.

---
+++ Alumni/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Alumni.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+    }
+
+    public class Alumni
+    {
+        protected Alumni()
+        {
+
+        }
+        public int UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string FieldOfStudy { get; set; }
+        public string JobPosition { get; set; }
+        public DateTime GraduationDate { get; set; }
+        public string Company { get; set; }
+    }
+}
